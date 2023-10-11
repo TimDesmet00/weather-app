1588bf103bb5dfd9c36ddf3380b714d662f2f03d
tim <timdesmet00@gmail.com>
2023-10-11 11:10:56 +0200

    tentative de correction de de débugage

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,67 @@
 const inputCity = document.getElementById("city");
 const btnCity = document.getElementById("btn-city");
 const dailyAverages = [];
+let apiKey;
 
 btnCity.addEventListener("click", () => {
   const cityChoice = inputCity.value;
   if (cityChoice) {
     localStorage.setItem("selectedCity", cityChoice);
-    // Effectuez ici l'appel à l'API OpenWeatherMap en utilisant la ville choisie (cityChoice)
+
     fetchWeatherData(cityChoice);
+
+    fetch("./config.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("La requête a échoué");
+        }
+        console.log(response.json);
+        return response.json();
+      })
+      .then((config) => {
+        console.log("clé de l'api: ", config.apiKey);
+        apiKey = config.apiKey;
+      })
+      .catch((error) => {
+        console.error(
+          "Erreur lors du chargement de la configuration : ",
+          error
+        );
+      });
   } else {
     alert("Veuillez entrer une ville.");
   }
 });
 
+// async function getApiKey() {
+//   try {
+//     const response = await fetch("./config.json");
+//     if (!response.ok) {
+//       throw new Error("Impossible de charger la configuration.");
+//     }
+//     const config = await response.json();
+//     console.log("cle API récupérée :", config.apiKey);
+//     if (config && config.apiKey) {
+//       return config.apiKey;
+//     } else {
+//       console.error("Erreur lors de l'accès à la clé API.");
+//       return null;
+//     }
+//   } catch (error) {
+//     console.error("Erreur lors du chargement de la configuration : ", error);
+//     return null;
+//   }
+// }
+
 async function fetchWeatherData(city) {
   try {
-    const apiKey = "8c8a54427b5d60c0d66c1eac58dbdc26";
+    const apiKey = await getApiKey();
+
+    if (!apiKey) {
+      console.error("La clé API est manquante ou invalide.");
+      return;
+    }
+
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
     const response = await fetch(apiUrl);
@@ -25,19 +71,15 @@ async function fetchWeatherData(city) {
 
     const data = await response.json();
 
-    // Effacez le contenu actuel de la section "answer-api"
     const answerApiSection = document.getElementById("answer-api");
     answerApiSection.innerHTML = "";
 
-    // Créez un objet pour stocker les prévisions par jour
     const forecastsByDay = {};
 
-    // Parcourez les données et regroupez-les par jour
     data.list.forEach((forecast) => {
       const date = new Date(forecast.dt * 1000);
       const dayKey = date.toLocaleDateString();
 
-      // Si la date n'existe pas dans l'objet, créez une nouvelle entrée
       if (!forecastsByDay[dayKey]) {
         forecastsByDay[dayKey] = [];
       }
@@ -45,17 +87,13 @@ async function fetchWeatherData(city) {
       forecastsByDay[dayKey].push(forecast);
     });
 
-    // Parcourez les prévisions regroupées par jour et calculez les moyennes
-    // const dailyAverages = [];
     for (const dayKey in forecastsByDay) {
       const forecastList = forecastsByDay[dayKey];
 
-      // Calcul de la moyenne quotidienne
       const averageTemperature = calculateDailyAverageTemperature(forecastList);
       const averageDescription = calculateDailyAverageDescription(forecastList);
       const averageHumidity = calculateDailyAverageHumidity(forecastList);
 
-      // Créez un objet de moyenne quotidienne
       const dailyAverage = {
         date: dayKey,
         temperature: averageTemperature,
@@ -66,7 +104,6 @@ async function fetchWeatherData(city) {
       dailyAverages.push(dailyAverage);
     }
 
-    // Affichez les moyennes journalières dans la section "answer-api"
     dailyAverages.forEach((dailyAverage) => {
       const averageDiv = createDailyAverageElement(dailyAverage);
       answerApiSection.appendChild(averageDiv);
@@ -78,8 +115,7 @@ async function fetchWeatherData(city) {
   }
 }
 
-// Fonction pour calculer la température moyenne par jour
-function calculateDailyAverageTemperature(forecastList) {
+const calculateDailyAverageTemperature = (forecastList) => {
   const totalTemperature = forecastList.reduce(
     (accumulator, forecast) => accumulator + forecast.main.temp,
     0
@@ -89,19 +125,15 @@ function calculateDailyAverageTemperature(forecastList) {
     1
   );
   return averageTemperatureCelcius;
-}
+};
 
-// Fonction pour calculer la description moyenne par jour
-function calculateDailyAverageDescription(forecastList) {
-  // Vous pouvez mettre en place votre propre logique pour déterminer la description moyenne
-  // Par exemple, en comptant combien de fois chaque description apparaît et en retournant la plus fréquente.
+const calculateDailyAverageDescription = (forecastList) => {
   const descriptionCounts = {};
   forecastList.forEach((forecast) => {
     const description = forecast.weather[0].description;
     descriptionCounts[description] = (descriptionCounts[description] || 0) + 1;
   });
 
-  // Trouver la description la plus fréquente
   let mostFrequentDescription = "";
   let maxCount = 0;
   for (const description in descriptionCounts) {
@@ -112,20 +144,18 @@ function calculateDailyAverageDescription(forecastList) {
   }
 
   return mostFrequentDescription;
-}
+};
 
-// Fonction pour calculer l'humidité moyenne par jour
-function calculateDailyAverageHumidity(forecastList) {
+const calculateDailyAverageHumidity = (forecastList) => {
   const totalHumidity = forecastList.reduce(
     (accumulator, forecast) => accumulator + forecast.main.humidity,
     0
   );
   const averageHumidity = totalHumidity / forecastList.length;
   return averageHumidity;
-}
+};
 
-// Fonction pour créer un élément de moyenne quotidienne
-function createDailyAverageElement(dailyAverage) {
+const createDailyAverageElement = (dailyAverage) => {
   const averageDiv = document.createElement("div");
   averageDiv.classList.add("daily-average");
 
@@ -149,15 +179,14 @@ function createDailyAverageElement(dailyAverage) {
   averageDiv.appendChild(humidityP);
 
   return averageDiv;
-}
+};
 
-// Fonction pour créer un élément d'information de prévision avec une classe spécifiée
-function createDailyInfoElement(text, className) {
+const createDailyInfoElement = (text, className) => {
   const element = document.createElement("p");
   element.textContent = text;
   element.classList.add(className);
   return element;
-}
+};
 
 window.addEventListener("load", () => {
   const savedCity = localStorage.getItem("selectedCity");
@@ -168,17 +197,13 @@ window.addEventListener("load", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Ciblez le bouton "Afficher le graphique" et ajoutez un gestionnaire d'événements
   const generateChartButton = document.getElementById("generateChart");
   generateChartButton.addEventListener("click", () => {
-    // Récupérez les données nécessaires pour le graphique
     const chartLabels = dailyAverages.map((average) => average.date);
     const temperatureData = dailyAverages.map((average) => average.temperature);
 
-    // Ciblez le canvas pour le graphique
     const ctx = document.getElementById("temperatureChart").getContext("2d");
 
-    // Créez le graphique en utilisant Chart.js
     new Chart(ctx, {
       type: "line",
       data: {
